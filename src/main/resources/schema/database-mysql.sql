5a0c21c0440cb4152531d45c143c4d8283b8ce19
donghyuck, son <andang.son@gmail.com>
2020-10-05 22:17:33 +0900

    mysql db, account, privileges sql update

--- src/main/resources/schema/database-mysql.sql
+++ src/main/resources/schema/database-mysql.sql
@@ -3,4 +3,4 @@ CREATE DATABASE islanddb CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 -- test 계성 생성
 CREATE USER 'studio'@'%' IDENTIFIED BY 'studio007A@';
 -- testdb에 대한 권한을 test 에게 부여한다. 
-GRANT ALL PRIVILEGES ON testdb.* TO 'studio'@'%' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON islanddb.* TO 'studio'@'%' WITH GRANT OPTION;
