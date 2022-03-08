565ebd6fc81ead0ecd9fce83c656444aee6ee5f7
donghyuck, son <andang.son@gmail.com>
2022-03-08 18:17:24 +0900

    Update VisionService.java

--- src/main/java/architecture/studio/service/VisionService.java
+++ src/main/java/architecture/studio/service/VisionService.java
@@ -1,6 +1,17 @@
 package architecture.studio.service;
 
+import java.io.File;
+
 public interface VisionService {
     
+    enum Classifier {
+        FACE,
+        LBP_FACE,
+        HAAR_FACE,
+        DNN_CAFFE_FACE,
+        EYES
+    }; 
+    
+    public void detect(File source , File target, Classifier classifier ) throws Exception;
 
 }
